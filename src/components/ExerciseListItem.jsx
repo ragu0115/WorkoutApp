cc6e6c68e6512398b9eb52a8eec6badcf0d01927
ragu0115 <raghu.palani01@gmail.com>
2024-04-24 15:37:19 -0700

    first

---
+++ src/components/ExerciseListItem.jsx
@@ -0,0 +1,45 @@
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+
+export default function ExerciseListItem({ item }) {
+  return (
+    <View style={styles.exerciseContainer}>
+        <Text style={styles.exerciseName}>{item.name}</Text>
+
+        <Text style={styles.exerciseSubtitle}>
+          <Text style={styles.subValue}>{item.muscle}</Text> |{' '}
+          <Text style={styles.subValue}>{item.equipment}</Text>
+        </Text>
+    </View>   
+  );
+}
+
+const styles = StyleSheet.create({
+  exerciseContainer: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 10,
+    gap: 5,
+    marginHorizontal: 2,
+
+    // shadow
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
+  },
+  exerciseName: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  exerciseSubtitle: {
+    color: 'red',
+  },
+  subValue: {
+    textTransform: 'capitalize',
+  },
+});
